4f40445268c830aec300e3a27df58b8d649c36e1
Michael Kriese <michael.kriese@visualon.de>
2019-05-09 16:54:15 +0200

    fix: throw error

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -24,16 +24,16 @@ $ErrorActionPreference = 'Stop'
 
 Write-Host testing node -ForegroundColor Green
 docker run --rm -t node sh -c 'yarn --version && pnpm --version'
-if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode)  }
+if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode); throw "test error" }
 
 Write-Host testing rancher-cli -ForegroundColor Green
 docker run --rm -t rancher-cli rancher --version
-if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode)  }
+if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode); throw "test error" }
 
 $images = @('dotnet-sdk', 'dotnet-aspnet')
 
 $images | ForEach-Object {
     Write-Host testing $_ -ForegroundColor Green
     docker run --rm -t $_ dotnet --info
-    if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode)  }
+    if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode); throw "test error" }
 }
