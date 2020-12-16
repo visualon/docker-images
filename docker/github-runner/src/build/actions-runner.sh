e3f404c63c4a32b477ffc1e0d3984efd81db3739
Michael Kriese <michael.kriese@visualon.de>
2020-12-16 11:49:25 +0100

    fix: replace dash

--- docker/github-runner/src/build/actions-runner.sh
+++ docker/github-runner/src/build/actions-runner.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-check_semver ${ACTIONS-RUNNER_VERSION}
+check_semver ${ACTIONS_RUNNER_VERSION}
 
 if [[ ! "${MAJOR}" || ! "${MINOR}" || ! "${PATCH}" ]]; then
-  echo Invalid version: ${ACTIONS-RUNNER_VERSION}
+  echo Invalid version: ${ACTIONS_RUNNER_VERSION}
   exit 1
 fi
 
@@ -13,7 +13,7 @@ mkdir -p /home/ubuntu/actions-runner
 cd /home/ubuntu/actions-runner
 
 DISTRO=linux-x64
-URL=https://github.com/actions/runner/releases/download/v${ACTIONS-RUNNER_VERSION}/actions-runner-${DISTRO}-${ACTIONS-RUNNER_VERSION}.tar.gz
+URL=https://github.com/actions/runner/releases/download/v${ACTIONS_RUNNER_VERSION}/actions-runner-${DISTRO}-${ACTIONS_RUNNER_VERSION}.tar.gz
 
 curl -sL $URL -o runner.tgz
 tar xzf runner.tgz
--- docker/github-runner/src/build/docker-compose.sh
+++ docker/github-runner/src/build/docker-compose.sh
@@ -2,15 +2,15 @@
 
 set -e
 
-check_semver ${DOCKER-COMPOSE_VERSION}
+check_semver ${DOCKER_COMPOSE_VERSION}
 
 if [[ ! "${MAJOR}" || ! "${MINOR}" || ! "${PATCH}" ]]; then
-  echo Invalid version: ${DOCKER-COMPOSE_VERSION}
+  echo Invalid version: ${DOCKER_COMPOSE_VERSION}
   exit 1
 fi
 
 DISTRO=Linux-x86_64
-URL=https://github.com/docker/compose/releases/download/${DOCKER-COMPOSE_VERSION}/docker-compose-${DISTRO}
+URL=https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-${DISTRO}
 TARGET=/usr/local/bin/docker-compose
 curl -sL $URL -o $TARGET
 chmod +x ${TARGET}
