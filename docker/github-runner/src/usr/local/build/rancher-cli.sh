9863d6339b0af832888294c89e2f991de95a33f5
Michael Kriese <michael.kriese@visualon.de>
2020-12-22 08:10:08 +0100

    fix: rancher extract

--- docker/github-runner/src/usr/local/build/rancher-cli.sh
+++ docker/github-runner/src/usr/local/build/rancher-cli.sh
@@ -9,11 +9,12 @@ if [[ ! "${MAJOR}" || ! "${MINOR}" || ! "${PATCH}" ]]; then
   exit 1
 fi
 
+# https://releases.rancher.com/cli/v0.6.14/rancher-linux-amd64-v0.6.14.tar.gz
 DISTRO=linux-amd64
 URL=https://releases.rancher.com/cli/v${RANCHER_CLI_VERSION}/rancher-${DISTRO}-v${RANCHER_CLI_VERSION}.tar.gz
 
-curl -sL $URL -o rancher.tgz
-tar xvzf -C /usr/local/bin/ --strip-components 2 rancher.tgz
-rm rancher.tgz
+curl -sL $URL -o tmp.tar.gz
+tar xzf -C /usr/local/bin/ --strip-components=2 tmp.tar.gz
+rm tmp.tar.gz
 
 rancher version
