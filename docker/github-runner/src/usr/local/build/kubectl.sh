aa0f34881d45223390118c90defbf7ca5ee7b9a0
Michael Kriese <michael.kriese@visualon.de>
2021-01-13 01:10:27 +0100

    feat: add kubectl

---
+++ docker/github-runner/src/usr/local/build/kubectl.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+check_semver ${KUBECTL_VERSION}
+
+if [[ ! "${MAJOR}" || ! "${MINOR}" || ! "${PATCH}" ]]; then
+  echo Invalid version: ${KUBECTL_VERSION}
+  exit 1
+fi
+
+DISTRO=linux/amd64
+URL=https://storage.googleapis.com/kubernetes-release/release/v${KUBECTL_VERSION}/bin/${DISTRO}/kubectl
+
+curl -sL $URL -o /usr/local/bin/kubectl
+chmod +x /usr/local/bin/kubectl
+
+kubectl version --client=true
