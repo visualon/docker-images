24865aa26d0432f82108d9bcbde0780f4d4f49d8
Michael Kriese <michael.kriese@visualon.de>
2021-01-21 17:17:40 +0100

    fix: pass args

--- windows/2019/builder/root/tools/bin/install-tool.cmd
+++ windows/2019/builder/root/tools/bin/install-tool.cmd
@@ -1,3 +1,3 @@
 @echo off
-PowerShell -noni -nop -c "$ErrorActionPreference = 'Stop'; install-tool.ps1 -Name '%1' -Version '%2'"
+PowerShell -noni -nop -c "$ErrorActionPreference = 'Stop'; install-tool.ps1 %*"
 if not "%ERRORLEVEL%" == "0" exit /B %ERRORLEVEL%
