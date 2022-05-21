1b960510cc530fa8a22c8eb5a27dbeb98aac39eb
Michael Kriese <michael.kriese@visualon.de>
2022-05-21 08:22:23 +0200

    fix(github-runner): support docker-compose v2

--- linux/github-runner/src/usr/local/buildpack/tools/docker-compose.sh
+++ linux/github-runner/src/usr/local/buildpack/tools/docker-compose.sh
@@ -2,17 +2,28 @@
 
 set -e
 
-check_semver ${DOCKER_COMPOSE_VERSION}
+check_semver "${DOCKER_COMPOSE_VERSION}"
 
 if [[ ! "${MAJOR}" || ! "${MINOR}" || ! "${PATCH}" ]]; then
-  echo Invalid version: ${DOCKER_COMPOSE_VERSION}
+  echo Invalid version: "${DOCKER_COMPOSE_VERSION}"
   exit 1
 fi
 
 DISTRO=Linux-x86_64
 URL=https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-${DISTRO}
-TARGET=/usr/local/bin/docker-compose
-curl -sL $URL -o $TARGET
-chmod +x ${TARGET}
 
-docker-compose version
+if [[ "${MAJOR}" -gt 1 ]]; then
+  TARGET=${HOME}/.docker/cli-plugins/docker-compose
+
+  mkdir -p "${HOME}"/.docker/cli-plugins
+
+  curl -sL "$URL" -o "${TARGET}"
+  chmod +x "${TARGET}"
+
+  docker compose version
+else
+  TARGET=/usr/local/bin/docker-compose
+  curl -sL "$URL" -o $TARGET
+  chmod +x ${TARGET}
+  docker-compose version
+fi
