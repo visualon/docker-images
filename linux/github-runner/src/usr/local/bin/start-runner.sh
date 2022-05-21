7047bacebcc113a74cc64056767754ec23bbd1db
Michael Kriese <michael.kriese@visualon.de>
2022-05-21 11:45:23 +0200

    fix(github-runner): wrong args
    - fixes #1300

--- linux/github-runner/src/usr/local/bin/start-runner.sh
+++ linux/github-runner/src/usr/local/bin/start-runner.sh
@@ -1,28 +1,30 @@
 #!/bin/bash
 
+set -e
+
 RUNNER_TOKEN=$(curl -sX POST -H "Authorization: token ${GITHUB_ACCESS_TOKEN}" https://api.github.com/repos/"${GITHUB_REPO}"/actions/runners/registration-token | jq .token --raw-output)
 
 if [[ -z $RUNNER_REPLACE_EXISTING ]]; then
     export RUNNER_REPLACE_EXISTING="true"
 fi
 
-CONFIG_OPTS=""
+CONFIG_OPTS=()
 
 if [ "$(echo $RUNNER_REPLACE_EXISTING | tr '[:upper:]' '[:lower:]')" == "true" ]; then
-	CONFIG_OPTS="--replace"
+	CONFIG_OPTS=("--replace")
 fi
 
 if [[ -n $RUNNER_LABELS ]]; then
-    CONFIG_OPTS="${CONFIG_OPTS} --labels ${RUNNER_LABELS}"
+    CONFIG_OPTS=("${CONFIG_OPTS[@]}" --labels "${RUNNER_LABELS}")
 fi
 
 if [[ -n $RUNNER_NAME ]]; then
-    CONFIG_OPTS="${CONFIG_OPTS} --name ${RUNNER_NAME}"
+    CONFIG_OPTS=("${CONFIG_OPTS[@]}"--name "${RUNNER_NAME}")
 fi
 
 ./config.sh \
   --url https://github.com/"${GITHUB_REPO}" \
-  --token "${RUNNER_TOKEN}" "$CONFIG_OPTS" \
+  --token "${RUNNER_TOKEN}" "${CONFIG_OPTS[@]}" \
   --work _work \
   ;
 
