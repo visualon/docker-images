fd661db3b147fb1705afc7174b74a41ed26e204d
Michael Kriese <michael.kriese@visualon.de>
2024-01-25 14:39:00 +0100

    feat: add docker-dind

---
+++ linux/docker-dind/push.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+FROM=$(grep 'FROM docker:' Dockerfile)
+SEMVER_REGEX=":(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)-dind)"
+
+
+if ! [[ "$FROM" =~ $SEMVER_REGEX ]]; then
+  echo Not a semver tag - skipping
+  exit
+fi
+
+major=${BASH_REMATCH[1]}
+minor=${BASH_REMATCH[2]}
+patch=${BASH_REMATCH[3]}
+
+
+tag="${major}.${minor}.${patch}"
+echo "Tagging ${IMAGE}:${tag}"
+docker tag "$IMAGE" "${IMAGE}:${tag}"
+docker push "${IMAGE}:${tag}"
